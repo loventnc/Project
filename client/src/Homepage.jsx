afc5615cad20d976e971362a815d0b062fc8af3c
loventnc <love.nattanischa2003@gmail.com>
2023-05-28 14:09:09 +0700

    Edit bottom

--- client/src/Homepage.jsx
+++ client/src/Homepage.jsx
@@ -1,6 +1,7 @@
 import group from '../src/image/group.png'
 import calender from '../src/image/calendar.png'
 import search from '../src/image/search.png'
+import { Link } from 'react-router-dom'
 
 const Homepage = () => {
 
@@ -13,12 +14,12 @@ const Homepage = () => {
         <h2 className='text-[20px]'>Reminder for study time, activities, and homework submissions  via LINE</h2>
         <div className='flex justify-center gap-5 rounded-lg mt-[70px]'>
           <img src={group} alt="" width="50px" />
-          <button className='text-[20px] bg-[#19A7CE] text-white px-10 py-2 rounded-lg hover:bg-[#7C96AB]'><a href="/Class">PUT SCHEDULE</a></button>
-          <button className='text-[20px] bg-[#19A7CE] text-white px-10 py-2 rounded-lg hover:bg-[#7C96AB]'><a href="/Homework">PUT HOMEWORK </a></button>
+          <button className='text-[20px] bg-[#19A7CE] text-white px-10 py-2 rounded-lg hover:bg-[#7C96AB]'><Link to='Class'>PUT SCHEDULE</Link></button>
+          <button className='text-[20px] bg-[#19A7CE] text-white px-10 py-2 rounded-lg hover:bg-[#7C96AB]'><Link to='Homework'>PUT HOMEWORK </Link></button>
           <img src={calender} alt="" width="50px" />
         </div>
         <div className='flex justify-center mt-5'>
-            <button className='text-[20px] bg-[#19A7CE] text-white px-10 py-2 rounded-lg hover:bg-[#7C96AB]'><a href="/Check">CHECK</a></button> 
+            <button className='text-[20px] bg-[#19A7CE] text-white px-10 py-2 rounded-lg hover:bg-[#7C96AB]'><Link to='Check'>CHECK</Link></button> 
           </div>
           <img className='pt-5' src={search} alt="" width="50px" />
       </div>
